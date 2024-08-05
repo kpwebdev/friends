bc4dfe7500e6d6cb125f973cf12a05059e7dafed
KP Web Dev <kpwebdevishere@gmail.com>
2024-08-05 16:44:14 +0530

    chagned PUT method to PATCH for friends model

--- src/routes/friends.route.js
+++ src/routes/friends.route.js
@@ -6,7 +6,7 @@ const friendsRouter = express.Router();
 friendsRouter.get("/", friendsController.getFriends);
 friendsRouter.get("/:id", friendsController.getFriend);
 friendsRouter.post("/", friendsController.postFriend);
-friendsRouter.put("/:id", friendsController.updateFriend);
+friendsRouter.patch("/:id", friendsController.updateFriend);
 friendsRouter.delete("/:id", friendsController.deleteFriend);
 
 module.exports = friendsRouter;
